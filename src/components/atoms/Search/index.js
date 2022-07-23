33a73a626b8f29be05a3692a7fbb2f2a6df8568c
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2022-07-23 22:55:25 +0200

    add handle search term

--- src/components/atoms/Search/index.js
+++ src/components/atoms/Search/index.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import "./_search.scss";
 
 const Search = () => {
+  const [citySearched, setCitySearched] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(
+      `Sorry, but ${citySearched} is not available. This is a demo app and it only shows 3 cities!`
+    );
+    setCitySearched("");
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSearch}>
       <Form.Label>Search</Form.Label>
       <Form.Group className="position-relative search-item">
         <Form.Control
           type="text"
           size="lg"
           placeholder="ex: Miami"
+          value={citySearched}
+          onChange={(e) => setCitySearched(e.target.value)}
         ></Form.Control>
         <Button
           className="position-absolute top-0 bg-btn-search"
